11b10c42846e6e28b0956da14309bd39eb1de685
Daun1la <102093990+Daun1la@users.noreply.github.com>
2024-09-12 10:34:02 +0300

    Update main.go

--- main.go
+++ main.go
@@ -8,6 +8,34 @@ import (
 	"strconv"
 )
 
+func digitMath (a string, b uint8, c int) string {
+	result := ""
+	if b == 42 {
+		for i := 1; i <= c; i++ {
+			result += a
+		}
+	}
+	if b == 47 {
+		result = a[:(len(a)/c)]
+	}
+	return result
+}
+
+func stringMath (a string, b uint8, c string) string {
+	result := ""
+	if b == 43 {
+		result = a + c
+	}
+	if b == 45 {
+		if len(c) > len(a) {
+			panic("Некорректная операция")
+		} else {
+			result = strings.ReplaceAll(a, c, "")
+		}
+	}
+	return result
+}
+
 func main() {
 	reader := bufio.NewReader(os.Stdin)
 	for {
